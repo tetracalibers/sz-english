83837357c01a5bc403100e9d27923f813c52864d
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-17 20:53:53 +0900

    aliasとbase設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,21 @@
+// @ts-check
+
 import { defineConfig } from "astro/config"
+import { resolve } from "node:path"
+const __dirname = new URL(".", import.meta.url).pathname
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig(
+  /** @type {import('astro').AstroUserConfig} */
+  {
+    vite: {
+      resolve: {
+        alias: {
+          "@": resolve(__dirname, "src")
+        }
+      }
+    },
+    site: "https://tetracalibers.github.io",
+    base: "/sz-english"
+  }
+)
