ebf7d4b30a5c4b2fdb8c63ef64580591b41bdb77
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-21 21:24:45 +0900

    単語リストテーブルブロック

---
+++ src/components/notion/table.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import type { RichTextItem } from "@/types/notion"
+  import RichText from "./rich-text.svelte"
+  import CellPhrase from "./cell-phrase.svelte"
+
+  type Content = {
+    table_width: number
+    children: {
+      type: string
+      table_row: {
+        cells: RichTextItem[][]
+      }
+    }[]
+  }
+
+  export let content: Content
+
+  const rows = content.children.map((ch) => ch.table_row)
+</script>
+
+<dl class="record-list">
+  {#each rows as row}
+    <div class="record">
+      <dt class="key">
+        <CellPhrase richtexts={row.cells[0]} />
+      </dt>
+      <dd class="value">
+        {#each row.cells[1] as item}
+          <RichText richtext={item} />
+        {/each}
+      </dd>
+    </div>
+  {/each}
+</dl>
+
+<style>
+  .record {
+    display: flex;
+    gap: 2rem;
+    justify-content: space-between;
+    border-bottom: 1px dashed #7c96ab;
+  }
+
+  .record-list {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin: 0;
+    padding: 0.5rem 1rem;
+  }
+
+  .record > * {
+    box-sizing: border-box;
+  }
+
+  .key {
+    line-height: 2;
+  }
+
+  .value {
+    margin: 0;
+    text-align: end;
+  }
+</style>
