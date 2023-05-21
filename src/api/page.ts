343b45d4e5ff9b40a3e09fc53b5dc7f7421581de
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-22 07:21:06 +0900

    再帰的にページ一覧を取得するスクリプト

--- src/api/page.ts
+++ src/api/page.ts
@@ -7,39 +7,49 @@ import {
 } from "@notionhq/client/build/src/api-endpoints"
 import fs from "node:fs/promises"
 
+type BlockObject = PartialBlockObjectResponse | BlockObjectResponse
+
+type PageTree = {
+  id: string
+  last_edited_time: string
+  title: string
+  children: PageTree[]
+}
+
 const notion = new Client({
   auth: import.meta.env.NOTION_TOKEN,
   logLevel: LogLevel.INFO
 })
 
-const pageId = "fb9b855e968c4a2a9437e46b5ad573f0"
+const rootId = "fb9b855e968c4a2a9437e46b5ad573f0"
 
-const getPage = async () => {
+const getPage = async (pageId: string) => {
   const res = await notion.blocks.children.list({ block_id: pageId })
   return res
 }
 
-const isChildPageBlock = (
-  block: PartialBlockObjectResponse | BlockObjectResponse
-): block is ChildPageBlockObjectResponse => {
+const isChildPageBlock = (block: BlockObject): block is ChildPageBlockObjectResponse => {
   if (!("type" in block)) return false
   if (block.type !== "child_page") return false
   return true
 }
 
-const collectChildPageList = (res: ListBlockChildrenResponse) => {
-  const { results } = res
-  return results
-    .map((block) => {
-      if (!isChildPageBlock(block)) return null
-      const { id, last_edited_time, child_page } = block
-      return { id, last_edited_time, title: child_page.title }
-    })
-    .filter(Boolean)
+const buildTree = async (block: BlockObject): Promise<PageTree> => {
+  if (!isChildPageBlock(block)) return null
+  const { id, last_edited_time, child_page } = block
+  const children = await getPage(id)
+  const subTree = await Promise.all(children.results.map(async (child) => await buildTree(child)))
+  return { id, last_edited_time, title: child_page.title, children: subTree.filter(Boolean) }
+}
+
+const collectChildPageList = async (res: ListBlockChildrenResponse) => {
+  const root = res.results
+  const tree = await Promise.all(root.map(async (block) => await buildTree(block)))
+  return tree.filter(Boolean)
 }
 
 export const fetchNotionPage = async () => {
-  const page = await getPage()
-  const childPages = collectChildPageList(page)
-  await fs.writeFile("data/root.json", JSON.stringify(childPages, null, 2))
+  const root = await getPage(rootId)
+  const childPages = await collectChildPageList(root)
+  await fs.writeFile("data/pages.json", JSON.stringify(childPages, null, 2))
 }
