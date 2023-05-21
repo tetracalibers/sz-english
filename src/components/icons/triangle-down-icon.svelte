fb47c8517b593965a34de3fb4592491a0f2682a2
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-21 21:21:50 +0900

    トグルアイコン

---
+++ src/components/icons/triangle-down-icon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { CSSLength } from "@/types/css-value"
+
+  export let size: CSSLength = "24px"
+</script>
+
+<svg style:--size={size} xmlns="http://www.w3.org/2000/svg" viewBox="7 8 8 6"
+  ><path fill="currentColor" d="m15 8l-4.03 6L7 8h8z" /></svg
+>
+
+<style>
+  svg {
+    width: var(--size);
+    height: var(--size);
+  }
+</style>
