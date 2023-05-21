151eed54f133c6d09a37dd5bc2bdc0deff95802b
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-21 21:22:11 +0900

    再生ボタン

---
+++ src/components/audio/play-button.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import PlayIcon from "../icons/play-icon.svelte"
+
+  export let play: () => void
+</script>
+
+<button on:click={play} aria-label="play">
+  <PlayIcon size="1rem" />
+</button>
+
+<style>
+  button {
+    width: 1.5rem;
+    height: 1.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    border: 0;
+    background-image: linear-gradient(135deg, #90f7ec 10%, #32ccbc 100%);
+    appearance: none;
+    cursor: pointer;
+  }
+</style>
