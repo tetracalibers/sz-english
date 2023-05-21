4950c80d0c2fa92ff4e841e05b4611ef3028a9d2
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-21 21:22:06 +0900

    停止ボタン

---
+++ src/components/audio/stop-button.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import StopIcon from "../icons/stop-icon.svelte"
+
+  export let stop: () => void
+</script>
+
+<button on:click={stop} aria-label="stop">
+  <StopIcon size="1rem" />
+</button>
+
+<style>
+  button {
+    width: 1.5rem;
+    height: 1.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    border: 0;
+    appearance: none;
+    cursor: pointer;
+    background-image: linear-gradient(135deg, #f6d242 10%, #ff52e5 100%);
+  }
+</style>
