0c8da064f39a0dce5224437082946dbf517211f8
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-21 21:22:24 +0900

    リッチテキスト

---
+++ src/components/notion/rich-text.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import type { RichTextItem } from "@/types/notion"
+
+  export let richtext: RichTextItem
+</script>
+
+<span
+  class={"color--" + richtext.annotations.color}
+  class:bold={richtext.annotations.bold}
+  class:italic={richtext.annotations.italic}
+>
+  {richtext.plain_text}
+</span>
+
+<style>
+  .color--pink {
+    color: #f266ab;
+  }
+
+  .color--purple {
+    color: #aa77ff;
+  }
+
+  .color--blue {
+    color: #3ab0ff;
+  }
+
+  .color-green {
+    color: #6bcb77;
+  }
+
+  .color-orange {
+    color: #f99b7d;
+  }
+
+  .color-yellow {
+    color: #fbc252;
+  }
+
+  .bold {
+    font-weight: bold;
+  }
+
+  .italic {
+    font-style: italic;
+  }
+</style>
