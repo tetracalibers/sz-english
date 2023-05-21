a948b7f05c7c3ea43dc1b1bc4acf3c60d05cfd37
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-21 21:24:18 +0900

    引用ブロック

---
+++ src/components/notion/quote.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { RichTextItem } from "@/types/notion"
+  import { QuoteBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints"
+  import RichText from "./rich-text.svelte"
+
+  type Content = {
+    rich_text: RichTextItem[]
+    color: QuoteBlockObjectResponse["quote"]["color"]
+  }
+
+  export let content: Content
+</script>
+
+<blockquote>
+  {#each content.rich_text as item}
+    <RichText richtext={item} />
+  {/each}
+</blockquote>
+
+<style>
+  blockquote {
+    margin-block: 0;
+    margin-inline-start: 2rem;
+    margin-inline-end: 1rem;
+    padding: 1rem;
+    box-shadow: 10px 0px 2px -10px currentColor inset;
+  }
+</style>
