020890ea7fc2e6818c55866805cd13e8a1c25324
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-21 21:24:05 +0900

    段落ブロック

---
+++ src/components/notion/paragraph.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { RichTextItem } from "@/types/notion"
+  import { ParagraphBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints"
+  import RichText from "./rich-text.svelte"
+
+  type Content = {
+    rich_text: RichTextItem[]
+    color: ParagraphBlockObjectResponse["paragraph"]["color"]
+  }
+
+  export let content: Content
+</script>
+
+<p>
+  {#each content.rich_text as item}
+    <RichText richtext={item} />
+  {/each}
+</p>
+
+<style>
+  p {
+    margin: 0;
+    text-indent: 1rem;
+  }
+</style>
