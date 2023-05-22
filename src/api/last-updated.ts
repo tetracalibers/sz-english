2fa516ef98ecea83fe56d08ffefbc1782bd24782
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-22 19:52:02 +0900

    最終更新日データ整形スクリプト

---
+++ src/api/last-updated.ts
@@ -0,0 +1,16 @@
+import { PageTree } from "./page"
+import fs from "node:fs/promises"
+import tree from "~/meta/pages.json" assert { type: "json" }
+
+const buildLastUpdatedMap = (tree: PageTree[], dist = new Map<string, string>()) => {
+  tree.forEach((page) => {
+    dist.set(page.id, page.last_edited_time)
+    if (page.children.length > 0) buildLastUpdatedMap(page.children, dist)
+  })
+  return Object.fromEntries(dist)
+}
+
+export const dumpLastUpdated = async () => {
+  const lastUpdatedMap = buildLastUpdatedMap(tree)
+  await fs.writeFile("meta/last-updated.json", JSON.stringify(lastUpdatedMap, null, 2))
+}
