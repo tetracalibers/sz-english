75e0a8c4f870d7996f079172f37abea0459a9461
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-22 20:54:02 +0900

    ログ表示ユーティリティ

---
+++ src/api/log.ts
@@ -0,0 +1,7 @@
+export const printFileUpdatedLog = (file: string) => {
+  console.log(`----- ${file} - Updated!! -----`)
+}
+
+export const printSkipLog = (label: string) => {
+  console.log(`-- Skip: ${label} --`)
+}
